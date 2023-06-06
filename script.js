737e9305fc3706be9ec5e82adedd8c92aa618829
cyberkesa <nastya63rus@mail.ru>
2023-06-07 01:49:58 +0300

    Added wrapper for height consistency

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ function createShapes() {
   const shapeContainer = document.querySelector(".container");
 
   if (shapeContainer) {
-    const numberArray = [...Array(12)].map((_, index) => index + 1);
+    const numberArray = Array.from({ length: 12 }, (_, index) => index + 1);
 
     numberArray.forEach((number) => {
-      const div = document.createElement("div");
-      div.className = `shape shape${number}`;
-      shapeContainer.appendChild(div);
+      const wrapperElement = document.createElement("div");
+      wrapperElement.className = "shape-wrapper";
+
+      const shapeElement = document.createElement("div");
+      shapeElement.className = `shape shape${number}`;
+
+      wrapperElement.appendChild(shapeElement);
+
+      shapeContainer.appendChild(wrapperElement);
     });
   } else {
     console.error("Container element not found!");
