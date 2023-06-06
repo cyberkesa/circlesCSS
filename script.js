1bcdf610e59803eedfdb3d934ac29ff2f812496d
cyberkesa <nastya63rus@mail.ru>
2023-06-06 03:55:45 +0300

    Refactor shape creation in container

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const arrayOfNumbers = [...Array(10)].map((_, index) => index + 1);
-arrayOfNumbers.map((a) => {
-  const div = document.createElement("div");
-  div.className = `shape shape${a}`;
-  document.querySelector(".container").appendChild(div);
-});
+function createShapes() {
+  const shapeContainer = document.querySelector(".container");
+
+  if (shapeContainer) {
+    const numberArray = [...Array(10)].map((_, index) => index + 1);
+
+    numberArray.forEach((number) => {
+      const div = document.createElement("div");
+      div.className = `shape shape${number}`;
+      shapeContainer.appendChild(div);
+    });
+  } else {
+    console.error("Container element not found!");
+  }
+}
+
+createShapes();
